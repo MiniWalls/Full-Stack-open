cfdef3d53fd4d7ea9b5e5feb733ab7675e430c05
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2023-01-02 21:30:20 +0200

    Completed exercise 1.7-1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const {good, neutral, bad} = props;
+  if(good === 0 && neutral === 0 && bad === 0){
+    return ( 
+      <p>No feedback given</p> 
+    )
+  }
+  return(
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>average {(1*good + 0*neutral + (-1)*bad)/(good + neutral + bad)}</p>
+      <p>positive {(good/(neutral + bad + good))*100}%</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,10 +30,8 @@ const App = () => {
       <button onClick={() => setGood(good + 1)} >good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
