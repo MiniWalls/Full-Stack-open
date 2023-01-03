3d09fca7e99756e20d2db46f3894a7e273c4d55c
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2023-01-04 00:08:01 +0200

    Completed exercise 2.4-2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,31 +2,22 @@ const Header = ({name}) => {
     return(
       <h1>{name}</h1>
     )
-  }
-  
-const Content = (props) => {
-return(
-    <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-    </div>
-)
 }
 
 const Part = (props) => {
-return(
-    <p>{props.part.name} {props.part.exercises}</p>
-)
+    return(
+        <p>{props.part.name} {props.part.exercises}</p>
+    )
 }
 
 const Total = (props) => {
     return(
-        <p>Number of exercises {props.parts.reduce((acc, part) => { return acc + part.exercises;}, 0)} </p>
+        <h3>Number of exercises {props.parts.reduce((acc, part) => { return acc + part.exercises;}, 0)} </h3>
     )
 }
 
 const Course = ({course}) => {
+    console.log(course)
     return (
         <>
             <Header name={course.name}></Header>
