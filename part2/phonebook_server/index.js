16135fd3b570fecd8dc39ae09b009d475dc31caa
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2023-01-17 20:31:01 +0200

    Completed exercises 3.2-3.5

--- part2/phonebook_server/index.js
+++ part2/phonebook_server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let addresses = [
     { 
       "id": 1,
@@ -27,6 +29,41 @@ let addresses = [
 
 app.get('/api/persons', (request, response) => {
     response.json(addresses)  
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const address = addresses.find(address => address.id === id)
+  if(address){
+    response.json(address)
+  } else {
+    response.status(404).end()
+  }
+  console.log(address)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  addresses = addresses.filter(address => address.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const id = Math.floor(Math.random() * 5000000)
+  const person = request.body
+  console.log(request.body)
+  person.id = id
+  addresses = addresses.concat(person)
+
+  response.json(person)
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date();
+  response.send(`<p>Phone book has info for ${addresses.length} people </br> ${date} </p>`)  
 })
 
 
