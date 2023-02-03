24848cbbe6791e3b7a838a718c2ce183d08d831b
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2023-02-03 22:16:46 +0200

    Completed exercises 4.13-4.14

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,16 +4,29 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 mongoose.set('strictQuery', false)
 
-const mongoUrl = config.URL
-mongoose.connect(mongoUrl)
+logger.info('connecting to', config.mongoUrl)
+mongoose.connect(config.URL)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -14,81 +14,143 @@ beforeEach(async () => {
     await blogObject.save()
   }
 })
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.type).toBe('application/json')
+  })
 
-test('blogs are returned as json', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.type).toBe('application/json')
+  test('all the blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blog id is defined', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('all the blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+describe('addition of a new blog', () => {
+  test('blog can be added', async() => {
+    const newBlog = {
+      title: 'Fishermans Diaries',
+      author: 'Arto kalastaja',
+      likes: 2,
+      url: 'http://www.kalastaja.fi'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(newBlog.title)
+  })
+
+  test('blog without likes defaults to 0', async() => {
+    const newBlog = {
+      title: 'Fishermans Diaries 2',
+      author: 'Arto kalastaja',
+      url: 'http://www.kalastaja2onparempi.fi'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
+  })
+
+  test('blog without title or url returns 400', async() => {
+    const newBlog = {
+      author: 'Arto kalastaja',
+      url: 'http://www.kalastaja2onparempi.fi',
+      likes: 2
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const newBlog2 = {
+      author: 'Arto kalastaja',
+      title: 'Fishing diaries 2',
+      likes: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog2)
+      .expect(400)
+  })
 })
 
-test('blog id is defined', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[0].id).toBeDefined()
+describe('deletion of a blog', () => {
+  test('blog can be deleted', async() => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length -1)
+  })
+  test('blog without valid id returns 400', async() => {
+    await api
+      .delete('/api/blogs/123')
+      .expect(400)
+  })
 })
 
-test('blog can be added', async() => {
-  const newBlog = {
-    title: 'Fishermans Diaries',
-    author: 'Arto kalastaja',
-    likes: 2,
-    url: 'http://www.kalastaja.fi'
-  }
+describe('change of existing blog', () => {
+  test('blog likes can be updated', async() => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const updatedBlog = {
+      title: blogToUpdate.title,
+      author: blogToUpdate.author,
+      url: blogToUpdate.url,
+      likes: blogToUpdate.likes + 1
+    }
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200)
 
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain(newBlog.title)
-})
+    const blogsAtEnd = await helper.blogsInDb()
 
-test('blog without likes defaults to 0', async() => {
-  const newBlog = {
-    title: 'Fishermans Diaries 2',
-    author: 'Arto kalastaja',
-    url: 'http://www.kalastaja2onparempi.fi'
-  }
+    expect(blogsAtEnd[0].likes).toBe(blogToUpdate.likes +1)
+  })
+  test('blog without valid id returns 400', async() => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
+    const updatedBlog = {
+      title: blogToUpdate.title,
+      author: blogToUpdate.author,
+      url: blogToUpdate.url,
+      likes: blogToUpdate.likes + 1
+    }
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
-})
-
-test('blog without title or url returns 400', async() => {
-  const newBlog = {
-    author: 'Arto kalastaja',
-    url: 'http://www.kalastaja2onparempi.fi',
-    likes: 2
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const newBlog2 = {
-    author: 'Arto kalastaja',
-    title: 'Fishing diaries 2',
-    likes: 3
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog2)
-    .expect(400)
+    await api
+      .put(`/api/blogs/${blogToUpdate.id+'123'}`)
+      .send(updatedBlog)
+      .expect(400)
+  })
 })
 
 afterAll(async () => {
